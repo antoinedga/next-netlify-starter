286f33523c89db560771e61d80a7ea6eaee599b7
Antoine Gordon-Alvarez <an.gordonalvarez@gmail.com>
2023-11-22 17:16:29 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my app saying sorry to Andrea!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I love you Andrea Bobadilla and then more and then more and then more and then more
         </p>
       </main>
 
